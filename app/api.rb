67ac8d9a8b4bba5706ee212f18a437c8a14b48a2
klaudiacode <oposyyy@gmail.com>
2022-10-31 14:45:34 +0800

    Add error case test

--- app/api.rb
+++ app/api.rb
@@ -3,9 +3,22 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base 
-    post '/expenses' do
-      JSON.generate('expense_id' => 42)
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
     end
+    post '/expenses' do
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
+    end
+    
     get '/expenses/:date' do
       JSON.generate([])
     end
